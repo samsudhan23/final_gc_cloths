ef2ae9460d3600675dd9a4155716121f7e645e06
Sudhan <sudhan.j@blindmatrix.com>
2025-05-28 20:15:40 +0530

    old signin

---
+++ src/app/pages/service/authentication.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { enviornment } from '../../../environment/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  private currentUserSubject = new BehaviorSubject<any>(null);
+
+  constructor(private http: HttpClient) {
+    if (typeof window !== 'undefined') {
+      const storedUser = localStorage.getItem('role');
+      if (storedUser) {
+        this.currentUserSubject.next(JSON.parse(storedUser));
+      }
+    }
+  }
+  getCurrentUser(): any {
+    return this.currentUserSubject.value;
+  }
+  login(user: any): Observable<any> {
+    return this.http.post<any>(enviornment.url + "auth/login", user).pipe(tap(res => {
+      if (typeof window !== 'undefined') {
+        localStorage.setItem('role', JSON.stringify(res.result));
+        this.currentUserSubject.next(res.result);
+      }
+    }));
+  }
+
+  reset(pass: any, token: any): Observable<any> {
+    return this.http.post<any>(enviornment.url + `auth/reset-password/${token}`, pass)
+  }
+}
