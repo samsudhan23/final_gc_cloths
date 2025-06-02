050a1f8936645deff86c76ba492af3c4be0ecb59
Sudhan <sudhan.j@blindmatrix.com>
2025-06-02 20:32:35 +0530

    user side

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -3,30 +3,22 @@ import { Notfound } from './app/pages/notfound/notfound';
 import { RoleGuard } from './app/shared/core/role.guard';
 import { SignInComponent } from './app/pages/auth/sign-in/sign-in.component';
 import { ResetpasswordComponent } from './app/pages/auth/resetpassword/resetpassword.component';
-import { HomeComponent } from './app/module/user_module/home/home.component';
+import { UserLayoutComponent } from './app/module/user_module/user-layout/user-layout.component';
 
 export const appRoutes: Routes = [
 
-    { path: '', component: HomeComponent },
+    { path: '', redirectTo: 'user', pathMatch: 'full' },
     {
         path: 'admin',
-        // component: AppLayout,
         loadChildren: () => import('./app/module/admin_module/admin.module').then(m => m.AdminModule),
         canActivate: [RoleGuard],
         data: { roles: ['admin'] }
-
-        // children: [
-        //     { path: '', component: Dashboard },
-        //     { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
-        //     { path: 'documentation', component: Documentation },
-        //     { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') }
-        // ]
     },
     {
         path: 'user',
         loadChildren: () => import('./app/module/user_module/user.module').then(m => m.UserModule),
-        canActivate: [RoleGuard],
-        data: { roles: ['user'] }
+        // canActivate: [RoleGuard],
+        // data: { roles: ['user'] }
     },
     { path: 'notfound', component: Notfound },
     {
--- src/app/module/user_module/user-routing.module.ts
+++ src/app/module/user_module/user-routing.module.ts
@@ -1,12 +1,9 @@
 import { RouterModule, Routes } from "@angular/router";
 import { NgModule } from "@angular/core";
-// import { Landing } from "../../pages/landing/landing";
 import { HomeComponent } from "./home/home.component";
-import { ShopComponent } from "./shop/shop.component";
 import { UserLayoutComponent } from "./user-layout/user-layout.component";
 
 export const routes: Routes = [
-    // { path: '', component: Landing }
     {
         path: '', component: UserLayoutComponent,
         children: [
