c6b989477430418883ce44912bd23a54ebff173c
Vimal2211 <unofficiald2211@gmail.com>
2025-09-22 00:00:52 +0530

    Update about and contact us page code on 22092025

--- src/app/module/user_module/user-footer/user-footer.component.ts
+++ src/app/module/user_module/user-footer/user-footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-footer',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './user-footer.component.scss'
 })
 export class UserFooterComponent {
+constructor(private router: Router,) {}
+  contactus(){
+    this.router.navigate(['user/contactus']);
+  }
+
+  aboutus(){
+    this.router.navigate(['user/about']);
+  }
 
 }
